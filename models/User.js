bee5d6e7c01558d21efd7cde5807566b2251c110
alexanderrathbone <alexander.thomas.rathbone@hotmail.com>
2021-04-26 17:49:41 +0200

    implement profile picture upload

--- models/User.js
+++ models/User.js
@@ -10,6 +10,7 @@ const userSchema = new Schema({
   firstName: String,
   lastName: String,
   description: String,
+  imgName: String,
   imgPath: String,
   publicId: String
 });
--- routes/profiles.js
+++ routes/profiles.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const Offer = require('../models/Offer');
+const { uploader, cloudinary } = require('../config/cloudinary');
 
 router.get('/', (req, res, next) => {
   const currentUser = req.user;
@@ -17,23 +18,29 @@ router.get('/edit', (req, res, next) => {
   })
 });
 
-router.post('/edit', (req, res, next) => {
+router.post('/edit', uploader.single('photo'), (req, res, next) => {
+  console.log('here is the file ' + req.file)
   const currentUser = req.user;
   const {
     firstName,
     lastName,
     description
   } = req.body;
-  // console.log(currentUser._id);
+  const imgPath = req.file.path;
+  const imgName = req.file.originalname;
+  const publicId = req.file.filename;
   User.findByIdAndUpdate(currentUser._id, {
-    firstName: firstName,
-    lastName: lastName,
-    description: description
-  })
+      firstName: firstName,
+      lastName: lastName,
+      description: description,
+      imgPath: imgPath,
+      imgName: imgName,
+      publicId: publicId
+    })
     .then(user => {
       res.redirect('/profiles');
     })
-    .catch(err=>{
+    .catch(err => {
       next(err);
     })
 })
