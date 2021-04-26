cc1960cca10eed8950959a9320de2383e2067c83
mhsngr <markus.heisinger@gmail.com>
2021-04-26 10:58:52 +0200

    create models

---
+++ models/Offer.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const offerSchema = new Schema({
+  name: {
+    type: String,
+  },
+  type: { 
+    type: String,
+    enum: ['appartment', 'room', 'other']
+  },
+  description: String,
+  adress: {
+    street: String,
+    number: Number,
+    code: Number,
+    city: {
+      type: String,
+      default: 'Berlin'
+    }
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  timeslots: [
+    {
+      time: Date,
+      status: {
+        type: String,
+        enum: ['free', 'booked']
+      },
+      bookedBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    }
+  ]
+});
+
+const Offer = model("Offer", offerSchema);
+
+module.exports = Offer;
