cf22d6c5c9fcfb9bd5e4645818a68191f66694aa
alexanderrathbone <alexander.thomas.rathbone@hotmail.com>
2021-04-26 18:34:23 +0200

    fix connection between profiles page and index, tidy up fields on index page to remove boilerplate

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const projectName = "transiRent";
 const capitalized = (string) =>
   string[0].toUpperCase() + string.slice(1).toLowerCase();
 
-app.locals.title = `${capitalized(projectName)} created with Ironlauncher`;
+app.locals.title = `${capitalized(projectName)}`;
 
 
 
