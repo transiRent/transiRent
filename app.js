928d1d2f1432cda1733beb3ab03e44f021680f1e
mhsngr <markus.heisinger@gmail.com>
2021-04-26 14:08:30 +0200

    create routes

--- app.js
+++ app.js
@@ -34,6 +34,9 @@ app.use("/", index);
 const auth = require('./routes/auth');
 app.use('/', auth);
 
+const offers = require("./routes/offers");
+app.use("/offers", offers);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
--- routes/offers.js
+++ routes/offers.js
@@ -4,7 +4,7 @@ const User = require('../models/User');
 const Offer = require('../models/Offer');
 
 router.get('/', (req, res, next) => {
-  Movie.find().populate('owner bookedBy')
+  Offer.find().populate('owner bookedBy')
     .then(offers => {
       res.render('offers/index', { offers });
     })
@@ -13,8 +13,39 @@ router.get('/', (req, res, next) => {
     });
 });
 
-router.get('/create', (req, res, next) => {
+router.get('/create', (req, res) => {
   res.render('offers/create');
+});
+
+router.post('/create', (req, res, next) => {
+  Offer.create(req.body)
+    .then(() => {
+      res.redirect('offers/index');
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+router.get('/:id', (req, res, next) => {
+  Offer.findById(req.params.id)
+    .then(offer => {
+      res.render('offers/view', { offer });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+
+router.post('/:id/book', (req, res, next) => {
+  Offer.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => {
+      res.redirect('/offers/index');
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 module.exports = router;
