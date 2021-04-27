e1a53d05c87149952e68e1f218013097db669aab
Jan Colomer <colojan7@gmail.com>
2021-04-27 19:53:15 +0200

    Add map locations, pop ups

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 const router = require("express").Router();
+const Offer = require('../models/Offer');
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   const currentUser = req.user;
   res.render("index", { user: currentUser });
 });
+
+/* GET data from database */
+router.get('/get-data', (req, res, next) => {
+  Offer.find()
+  .then(allOffers => {
+    console.log(allOffers)
+    res.json(allOffers);
+  })
+  .catch(err => {
+    next(err)
+  }) 
+})
 
 module.exports = router;
