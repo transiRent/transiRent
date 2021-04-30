c85de107f902deda6fb04a33903b49fc943466dc
mhsngr <markus.heisinger@gmail.com>
2021-04-30 08:53:10 +0200

    console log

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -37,7 +37,7 @@ const mapboxClient = mapboxSdk({ accessToken: mapboxgl.accessToken });
 axios.get('/get-data')
    .then(res => {
       const offers = organizeData(res.data)
-
+      console.log(offers);
       function addMarkers() {
          offers.features.forEach(function (marker) {
             var el = document.createElement('div');
